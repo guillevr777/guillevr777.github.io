1b6cf923162e1e6ff22b4eca0cff44cdb8351ac0
guillevr777 <gs.villanueva@iesnervion.es>
2025-02-25 20:59:22 +0100

    Create BoletinEmpresa.sql

---
+++ HTML/BBDD/BoletinEmpresa.sql
@@ -0,0 +1,37 @@
+--Consultas multitablas
+--53. Mostrar la información de los empleados junto a la información de las oficinas en las que trabajan.
+
+SELECT 
+--54. Igual que el ejercicio anterior, pero mostrando solo los empleados que trabajan en las oficinas del sur.
+
+--55. Igual que el ejercicio anterior, ordenando los empleados por edad.
+
+--56. Escribir una consulta que muestre la información de todos los empleados junto a los datos de su oficina.
+
+--57. Escribir una consulta que muestre la información de todos los empleados (trabajen o no en una oficina) junto a la información de su oficina.
+
+--58. Listar las oficinas del este indicando para cada una de ellas su número, ciudad, números y nombres de sus empleados. Hacer una versión en la que aparecen sólo las que tienen empleados, y hacer otra en las que además aparezcan las oficinas del este que no tienen empleados.
+
+--59. Listar los pedidos mostrando su número de pedido e importe, junto a la información del cliente (nombre y límite de crédito) que realiza el pedido.
+
+--60. Igual que el ejercicio anterior mostrando además la información del empleado que fue responsable de este pedido.
+
+--61. Mostrar para cada producto la información de sus pedidos
+
+--62. Listar los datos de cada uno de los empleados, la ciudad y región en donde trabaja.
+
+--63. Listar todas las oficinas con objetivo superior a 60.000 euros indicando para cada una de ellas el nombre de su director.
+
+--64. Listar los pedidos superiores a 2.500 euros, incluyendo el nombre del empleado responsable del pedido y el nombre del cliente que lo solicitó. Ordenar la consulta por el nombre del cliente.
+
+--65. Listar ordenados por el nombre los empleados que han realizado algún pedido.
+
+--66. Hallar los empleados que realizaron su primer pedido el mismo día que fueron contratados.
+
+--67. Listar los empleados con una cuota superior a la de su jefe; para cada empleado sacar sus datos y el número, nombre y cuota de su jefe.
+
+--68. Listar los números de los empleados que son responsables de un pedido superior a 1.000 euros o que tengan una cuota inferior a 5.000 euros.
+
+--69. Mostrar las oficinas que no tienen director o que se encuentran en la región sur.
+
+--70. Listar las oficinas que no tienen director o en las que trabaja alguien.
